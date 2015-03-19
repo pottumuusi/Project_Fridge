5c27f39366bdc26f24ec546b46bb915701b86b0b
pottumuusi <backman-93@hotmail.com>
2015-03-20 00:56:56 +0200

    committing before branch

--- fridge/Fridge.java
+++ fridge/Fridge.java
@@ -41,9 +41,9 @@ public class Fridge{
     return MAE_handler;
   }
   
-  /*public void MAE_brokerCall(String eventSource, String className){
+  public void MAE_brokerCall(String eventSource, String className){
     MAE_broker.solveAction(eventSource, className, MAE_handler);
-  }*/
+  }
   
   /*public void MAE_handlerCall(){
     
